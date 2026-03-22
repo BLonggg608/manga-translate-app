6ecea1040a167204ff64441139dd6323d8f1391c
Blonggg608 <tranhabaolong608@gmail.com>
2026-03-22 23:50:04 +0700

    update build script to use robocopy for internal files

--- build_exe.cmd
+++ build_exe.cmd
@@ -5,4 +5,4 @@ set "SOURCE_DIR=%PROJECT_ROOT%dist\MangaTranslator"
 set "TARGET_DIR=E:\manga-translator-app"
 
 copy /Y "%SOURCE_DIR%\MangaTranslator.exe" "%TARGET_DIR%\" >nul
-copy "%SOURCE_DIR%\_internal" "%TARGET_DIR%\_internal" /MIR /NDL /NFL /NJH /NJS
+robocopy "%SOURCE_DIR%\_internal" "%TARGET_DIR%\_internal" /MIR /NDL /NFL /NJH /NJS
